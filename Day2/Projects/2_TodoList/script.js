997b5bc3007ad37c7392e2f8a8c8d8cf32ed2d72
Abr009 <bradithya945@gmail.com>
2025-07-15 09:54:19 +0530

    Updated TodoApp script

--- Day2/Projects/2_TodoList/script.js
+++ Day2/Projects/2_TodoList/script.js
@@ -16,12 +16,43 @@ const display = (task) => {
   if (counter == 1)
     taskCont.innerHTML = ""
 
-  taskCont.innerHTML += `
-    <div id="task${counter}" class="w-full h-14 bg-gray-100 flex items-center justify-between px-4 py-7 gap-2">
-      <p onclick="taskDone('task${counter}')" class="font-semibold text-gray-700 cursor-pointer">${task}</p>
-      <i onclick="deleteTask('task${counter}')" class="ri-delete-bin-line text-white bg-red-500 px-2 py-1 rounded-lg text-xl cursor-pointer"></i>
-    </div>
-  `;
+  // Storing id name in variable
+  const currentId = `task${counter}`;
+
+  // Creating Element
+  let taskBody = document.createElement('div');
+  let taskName = document.createElement('p');
+  let deleteIcon = document.createElement('i');
+
+  // Adding ClassNames
+  taskBody.classList = "w-full h-14 bg-gray-100 flex items-center justify-between px-4 py-7 gap-2"
+  taskName.classList = "font-semibold text-gray-700 cursor-pointer"
+  deleteIcon.classList = "ri-delete-bin-line text-white bg-red-500 px-2 py-1 rounded-lg text-xl cursor-pointer"
+
+  // Adding id to taskBody
+  taskBody.id = currentId
+
+  // Adding textContent to p tag
+  taskName.textContent = task
+
+  // Adding Event Listeners
+  taskName.addEventListener("click", () => taskDone(currentId))
+  deleteIcon.addEventListener("click", () => deleteTask(currentId))
+
+  // Inserting taskName and deleteIcon to taskBody
+  taskBody.appendChild(taskName);
+  taskBody.appendChild(deleteIcon);
+
+  // Inserting taskBody inside taskContainer
+  taskCont.appendChild(taskBody);
+
+  // Just for Reference
+  // taskCont.innerHTML += `
+  //   <div id="task${counter}" class="w-full h-14 bg-gray-100 flex items-center justify-between px-4 py-7 gap-2">
+  //     <p onclick="taskDone('task${counter}')" class="font-semibold text-gray-700 cursor-pointer">${task}</p>
+  //     <i onclick="deleteTask('task${counter}')" class="ri-delete-bin-line text-white bg-red-500 px-2 py-1 rounded-lg text-xl cursor-pointer"></i>
+  //   </div>
+  // `;
 
   counter++;
 }
